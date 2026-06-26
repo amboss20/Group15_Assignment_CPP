ce1cd1521820351897574f687250fd3200a7b487
Syahir-Code <sy4h.0fficial@gmail.com>
2026-06-27 01:44:48 +0800

    Created console interface

--- main.cpp
+++ main.cpp
@@ -36,8 +36,42 @@ class FlashCard {
     }
 };
 
-int main() {
+void print (string str) {
+    cout << str << endl;
+}
 
+int main() {
+    // sample flashcard object
+    FlashCard c1;
+    int choice=0;
+
+    do {
+        
+        cout << endl;
+        print ("---------------------------------");
+        print ("      FLASHCARD CONSOLE          ");
+        print ("---------------------------------");
+        print ("1. Add FlashCard");
+        print ("2. Review Flashcards");
+        print ("3. View Progress");
+        print ("4. Exit");
+        cout << endl;
+
+        cout << "Enter your choice (number): ";
+        cin >> choice;
+        cin.ignore();
+        cout << endl;
+
+        if (choice == 1) {
+            c1.setQuestion();
+            c1.setAnswer();
+        }
+
+        else if  (choice == 4) {
+            return 0;
+        }
+
+    } while (true);
 
     return 0;
 }
