fd16a2bf3c478895dddc769e736e3aa7fc3e46b0
Syahir-Code <sy4h.0fficial@gmail.com>
2026-06-27 03:00:33 +0800

    Created class UserProgress, cmd shortcuts

--- main.cpp
+++ main.cpp
@@ -25,6 +25,8 @@ class FlashCard {
 
     string getAnswer () { return answer; }
 
+    int getScore () { return score; }
+
     int increaseScore () {
         score++;
         return score;
@@ -36,6 +38,23 @@ class FlashCard {
     }
 };
 
+class UserProgress {
+    private:
+    FlashCard *card;
+
+    public:
+    UserProgress (FlashCard *c) { card=c; }
+
+    void progress () {
+        cout << "User Progress:" << endl;
+        cout << "Question: " << card->getQuestion() << endl;
+        cout << "Point: " << card->getScore() << endl;
+        cout << endl;
+    }
+
+
+};
+
 void print (string str) {
     cout << str << endl;
 }
@@ -43,6 +62,8 @@ void print (string str) {
 int main() {
     // sample flashcard object
     FlashCard c1;
+    // sample object to track progress
+    UserProgress track (&c1);
     int choice=0;
 
     do {
@@ -65,6 +86,19 @@ int main() {
         if (choice == 1) {
             c1.setQuestion();
             c1.setAnswer();
+            // score increment test
+            c1.increaseScore();
+        }
+
+        else if (choice == 2) {
+            cout << "Still in development..." << endl;
+            cout << "To be developed by Ambriss..." << endl;
+            // score decrement test
+            c1.decreaseScore();
+        }
+
+        else if (choice == 3) {
+            track.progress();
         }
 
         else if  (choice == 4) {
