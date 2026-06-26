98e5f8712e36b3d98d0a504a21b5fdc683d05c8b
Syahir-Code <sy4h.0fficial@gmail.com>
2026-06-26 21:32:52 +0800

    Syahir created FlashCard class

--- main.cpp
+++ main.cpp
@@ -2,7 +2,74 @@
 #include <string.h>
 using namespace std;
 
-int main() {
+class FlashCard {
+    private:
+    string question;
+    string answer;
+    static int score;
 
+    public:
+    FlashCard () { }
+
+    void setQuestion () {
+        cout << "Add a question: ";
+        getline (cin, question);
+    }
+
+    void setAnswer () {
+        cout << "Add the answer: ";
+        getline (cin, answer);
+    }
+
+    void prompt () {
+        string userAns;
+        cout << "Question: ";
+        cout << question << endl;
+
+        cout << "Enter your answer: ";
+        getline (cin, userAns);
+        cout<<endl;
+
+        if (userAns == answer) {
+            increaseScore();
+        }
+
+        else {
+            decreaseScore();
+            if (score<0)  {score=0;}
+        }
+
+        cout << "Correct answer: ";
+        cout << answer << endl;
+        cout << "Score: ";
+        cout << score << endl;
+    }
+
+    string getQuestion () {return question;}
+
+    string getAnswer () {return answer;}
+
+    int increaseScore () {
+        score++;
+        return score;
+    }
+
+    int decreaseScore () {
+        score--;
+        return score;
+    }
+};
+
+int FlashCard::score = 0;
+
+int main() {
+    // sample of class FlashCard
+    FlashCard c1;
+    cout << endl;
+    c1.setQuestion();
+    c1.setAnswer();
+    cout << endl;
+
+    c1.prompt();
     return 0;
 }
